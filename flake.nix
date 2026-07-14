04534fd0d9111d849deac25a7d2d87d6784494b9
Yogansh Sharma <yogansh@yogansh.tech>
2026-07-14 23:52:57 +0530

    flake update

--- flake.nix
+++ flake.nix
@@ -3,24 +3,64 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = { self, nixpkgs }:
-  let 
-    pkgs = nixpkgs.legacyPackages.x86_64-linux; 
-  in 
-  {
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    treefmt-nix,
+    git-hooks,
+    ...
+  }: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f system nixpkgs.legacyPackages.${system});
 
-    packages.x86_64-linux.default = pkgs.writeShellApplication {
-      name="sz";
-      runtimeInputs = with pkgs; [
-        skim
-        fzf
-        tmux
-      ];
-      text = builtins.readFile ./tmux-sessionizer;
+    treefmtEval = eachSystem (
+      system: pkgs:
+        treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+          programs.shellcheck.enable = true;
+        }
+    );
+  in {
+    packages = eachSystem (system: pkgs: {
+      default = pkgs.writeShellApplication {
+        name = "sz";
+        runtimeInputs = with pkgs; [skim fzf tmux];
+        text = builtins.readFile ./tmux-sessionizer;
+      };
+    });
 
-    };
+    formatter = eachSystem (system: pkgs: treefmtEval.${system}.config.build.wrapper);
 
+    checks = eachSystem (system: pkgs: {
+      treefmt = treefmtEval.${system}.config.build.check ./.;
+      pre-commit-check = git-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          shellcheck.enable = true;
+        };
+      };
+    });
+
+    devShells = eachSystem (system: pkgs: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [shellcheck shfmt];
+      };
+    });
+
+    apps = eachSystem (system: pkgs: {
+      default = {
+        type = "app";
+        program = "${nixpkgs.lib.getExe self.packages.${system}.default}";
+      };
+    });
   };
 }
